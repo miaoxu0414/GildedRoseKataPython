6985a1a7c8fb6f9d7def7597efda110800575184
miaoxu0414 <xumiaosdfz@gmail.com>
2025-02-05 21:31:55 -0800

    add comments

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,43 +1,36 @@
 import unittest
 from gilded_rose import Item, GildedRose
 
+
 class GildedRoseTest(unittest.TestCase):
 
-    def test_normal_item_quality_decreases(self):
-        """Test that normal items decrease in quality each day"""
-        items = [Item("Elixir of the Mongoose", 5, 7)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        
-        self.assertEqual(items[0].sell_in, 4)  # Sell-in decreases
-        self.assertEqual(items[0].quality, 6)  # Quality should decrease
-
+    # Logical error test: Aged Brie should increase in quality by 1 each day
     def test_aged_brie_increases_in_quality(self):
-        """Test that Aged Brie increases in quality as it ages"""
         items = [Item("Aged Brie", 2, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        
-        self.assertEqual(items[0].sell_in, 1)
-        self.assertEqual(items[0].quality, 1)  # Quality should increase
+        self.assertEqual(items[0].quality, 2)  # Intentionally incorrect (actual should be 1)
 
+    # Logical error test: Backstage passes should drop to 0 quality after the concert
     def test_backstage_passes_drop_to_zero_after_concert(self):
-        """Test that Backstage passes' quality drops to zero after concert"""
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        
-        self.assertEqual(items[0].sell_in, -1)
-        self.assertEqual(items[0].quality, 0)  # Quality drops to zero
+        self.assertEqual(items[0].quality, 1)  # Intentionally incorrect (actual should be 0)
 
-    def test_call_non_existent_method(self):
-        """Syntax Error: Trying to call a method that does not exist"""
-        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
+    # Logical error test: Normal items should decrease in quality by 1 each day
+    def test_normal_item_quality_decreases(self):
+        items = [Item("Elixir of the Mongoose", 5, 7)]
         gilded_rose = GildedRose(items)
-        
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 4)  # Intentionally incorrect (actual should be 6)
+
+    # Syntax error test: Calling a non-existent method should raise an AttributeError
+    def test_call_non_existent_method(self):
+        items = [Item("Sulfuras", 5, 80)]
+        gilded_rose = GildedRose(items)
         with self.assertRaises(AttributeError):
-            gilded_rose.get_items()  # Method does not exist
+            gilded_rose.get_items()  # This method does not exist
 
 if __name__ == '__main__':
     unittest.main()
-
