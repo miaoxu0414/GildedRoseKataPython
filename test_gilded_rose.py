2512176efba8aae9d298a29b8b7a8fd297c4d755
miaoxu0414 <xumiaosdfz@gmail.com>
2025-02-05 21:19:51 -0800

    Write failed test

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,29 +1,43 @@
-# -*- coding: utf-8 -*-
 import unittest
-
 from gilded_rose import Item, GildedRose
 
-
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-    # example of test that checks for logical errors
-    def test_sulfuras_should_not_decrease_quality(self):
-        items = [Item("Sulfuras", 5, 80)]
+
+    def test_normal_item_quality_decreases(self):
+        """Test that normal items decrease in quality each day"""
+        items = [Item("Elixir of the Mongoose", 5, 7)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
-        sulfuras_item = items[0]
-        self.assertEquals(80, sulfuras_item.quality)
-        self.assertEquals(4, sulfuras_item.sell_in)
-        self.assertEquals("Sulfuras", sulfuras_item.name)
-    # example of test that checks for syntax errors
-    def test_gilded_rose_list_all_items(self):
-        items = [Item("Sulfuras", 5, 80)]
-        gilded_rose = GildedRose(items)
-        all_items = gilded_rose.get_items()
-        self.assertEquals(["Sulfuras"], all_items)
+        
+        self.assertEqual(items[0].sell_in, 4)  # Sell-in decreases
+        self.assertEqual(items[0].quality, 6)  # Quality should decrease
 
+    def test_aged_brie_increases_in_quality(self):
+        """Test that Aged Brie increases in quality as it ages"""
+        items = [Item("Aged Brie", 2, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        
+        self.assertEqual(items[0].sell_in, 1)
+        self.assertEqual(items[0].quality, 1)  # Quality should increase
+
+    def test_backstage_passes_drop_to_zero_after_concert(self):
+        """Test that Backstage passes' quality drops to zero after concert"""
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        
+        self.assertEqual(items[0].sell_in, -1)
+        self.assertEqual(items[0].quality, 0)  # Quality drops to zero
+
+    def test_call_non_existent_method(self):
+        """Syntax Error: Trying to call a method that does not exist"""
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
+        gilded_rose = GildedRose(items)
+        
+        with self.assertRaises(AttributeError):
+            gilded_rose.get_items()  # Method does not exist
 
 if __name__ == '__main__':
     unittest.main()
+
