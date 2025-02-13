43d18c495413a35d689bd81252d3e4c09be320bb
miaoxu0414 <xumiaosdfz@gmail.com>
2025-02-12 19:58:59 -0800

    add comments

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -4,33 +4,34 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
 
-    # Logical error test: Aged Brie should increase in quality by 1 each day
+    # Test: Aged Brie should increase in quality by 1 each day
     def test_aged_brie_increases_in_quality(self):
         items = [Item("Aged Brie", 2, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(items[0].quality, 2)  # Intentionally incorrect (actual should be 1)
+        self.assertEqual(items[0].quality, 1)  
 
-    # Logical error test: Backstage passes should drop to 0 quality after the concert
+    # Test: Backstage passes should drop to 0 quality after the concert
     def test_backstage_passes_drop_to_zero_after_concert(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(items[0].quality, 1)  # Intentionally incorrect (actual should be 0)
+        self.assertEqual(items[0].quality, 0)  
 
-    # Logical error test: Normal items should decrease in quality by 1 each day
+    # Test: Normal items should decrease in quality by 1 each day
     def test_normal_item_quality_decreases(self):
         items = [Item("Elixir of the Mongoose", 5, 7)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(items[0].quality, 4)  # Intentionally incorrect (actual should be 6)
+        self.assertEqual(items[0].quality, 6)  
 
-    # Syntax error test: Calling a non-existent method should raise an AttributeError
+    # Test: Calling a non-existent method should raise an AttributeError
     def test_call_non_existent_method(self):
         items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         with self.assertRaises(AttributeError):
-            gilded_rose.get_items()  # This method does not exist
+            gilded_rose.get_items()  
+
 
 if __name__ == '__main__':
     unittest.main()
